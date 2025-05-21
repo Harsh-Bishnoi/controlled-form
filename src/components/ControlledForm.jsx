8be9217fb3896e677d27bed72da31f21798c3df0
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-05-21 16:01:05 +0530

    chanhe in code

--- src/components/ControlledForm.jsx
+++ src/components/ControlledForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import Input from './common/Input';
 import Button from './common/Button';
 
@@ -9,55 +9,64 @@ const ControlledForm = () => {
         email: "",
         password: "",
         confirmPassword: ""
-    }
-    const [InputValue, setInputValue] = useState(initialValue);
+    };
+
+    const [inputValue, setInputValue] = useState(initialValue);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Form submitted:", inputValue);
+    };
+
     return (
         <>
             <div className="flex min-h-screen items-center justify-center">
                 <div className="max-w-[1140px] w-full px-4 mt-4">
                     <div className="font-mono font-semibold text-4xl text-center text-gray-500 pb-10 sm:text-6xl">Controlled Form</div>
-                    <div className="flex flex-col sm:flex-row justify-center items-center gap-4">
+                    <form onSubmit={handleSubmit}>
+                        <div className="flex flex-col sm:flex-row justify-center items-center gap-4">
+                            <Input
+                                name="firstName"
+                                value={inputValue.firstName}
+                                type="text"
+                                placeholder="First Name"
+                                onchange={(e) => setInputValue({ ...inputValue, firstName: e.target.value })} />
+                            <Input
+                                name="lastName"
+                                value={nputValue.lastName}
+                                type="text"
+                                placeholder="Last Name"
+                                onchange={(e) => setInputValue({ ...inputValue, lastName: e.target.value })} />
+                        </div>
                         <Input
-                            name="firstName"
-                            value={InputValue.firstName}
-                            type="text"
-                            placeholder="First Name"
-                            onchange={(e) => setInputValue({ ...InputValue, firstName: e.target.value })} />
-                        <Input
-                            name="lastName"
-                            value={InputValue.lastName}
-                            type="text"
-                            placeholder="Last Name"
-                            onchange={(e) => setInputValue({ ...InputValue, lastName: e.target.value })}
-                        />
-                    </div>
-                    <Input
-                        inputClass="sm:max-w-[816px] mx-auto flex mt-4"
-                        name="email"
-                        value={InputValue.email}
-                        type="email"
-                        placeholder="Email"
-                        onchange={(e) => setInputValue({ ...InputValue, email: e.target.value })} />
-                    <div className="flex flex-col sm:flex-row justify-center items-center gap-4 mt-4">
-                        <Input
-                            name="password"
-                            value={InputValue.password}
-                            type="Password"
-                            placeholder="Password"
-                            onchange={(e) => setInputValue({ ...InputValue, password: e.target.value })} />
-                        <Input
-                            name="confirmPassword"
-                            value={InputValue.confirmPassword}
-                            type="Password"
-                            placeholder="Confirm Password"
-                            onchange={(e) => setInputValue({ ...InputValue, confirmPassword: e.target.value })}
-                        />
-                    </div>
-                    <Button type="Submit" text="Submit" />
+                            inputClass="sm:max-w-[816px] mx-auto flex mt-4"
+                            name="email"
+                            value={inputValue.email}
+                            type="email"
+                            placeholder="Email"
+                            onchange={(e) => setInputValue({ ...inputValue, email: e.target.value })} />
+                        <div className="flex flex-col sm:flex-row justify-center items-center gap-4 mt-4">
+                            <Input
+                                name="password"
+                                value={inputValue.password}
+                                type="password"
+                                placeholder="Password"
+                                onchange={(e) => setInputValue({ ...inputValue, password: e.target.value })} />
+                            <Input
+                                name="confirmPassword"
+                                value={inputValue.confirmPassword}
+                                type="password"
+                                placeholder="Confirm Password"
+                                onchange={(e) => setInputValue({ ...inputValue, confirmPassword: e.target.value })} />
+                        </div>
+                        <div className="flex justify-center mt-6">
+                            <Button type="submit" text="Submit" />
+                        </div>
+                    </form>
                 </div>
             </div>
         </>
     )
 }
 
-export default ControlledForm
+export default ControlledForm;
