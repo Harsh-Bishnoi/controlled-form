66c9d53bb1bcc567fa17e0cac22bac54da31fd78
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-05-21 12:36:30 +0530

    make components

---
+++ src/components/ControlledForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ControlledForm = () => {
+    return (
+        <div>ControlledForm</div>
+    )
+}
+
+export default ControlledForm
---
+++ src/components/UnControlledForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UnControlledForm = () => {
+    return (
+        <div>UnControlledForm</div>
+    )
+}
+
+export default UnControlledForm
