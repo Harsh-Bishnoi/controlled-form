56583516212fc90abf67796bac5335ca360b322e
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-05-21 12:49:41 +0530

    add uncontrolled form

--- src/components/UnControlledForm.jsx
+++ src/components/UnControlledForm.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Input from './common/Input';
+import Button from './common/Button';
 
 const UnControlledForm = () => {
     return (
-        <div>UnControlledForm</div>
+        <>
+            <div className="flex min-h-screen items-center justify-center">
+                <div className="max-w-[1140px] w-full px-4 mt-4">
+                    <div className="font-mono font-semibold text-4xl text-center text-gray-500 pb-10 sm:text-6xl">Uncontrolled Form</div>
+                    <div className="flex flex-col sm:flex-row justify-center items-center gap-4">
+                        <Input type="text" placeholder="First Name" />
+                        <Input type="text" placeholder="Last Name" />
+                    </div>
+                    <Input inputClass="sm:max-w-[816px] mx-auto flex mt-4" type="email" placeholder="Email" />
+                    <div className="flex flex-col sm:flex-row justify-center items-center gap-4 mt-4">
+                        <Input type="Password" placeholder="Password" />
+                        <Input type="Password" placeholder="Confirm Password" />
+                    </div>
+                    <Button type="Submit" text="Submit" />
+                </div>
+            </div>
+        </>
     )
 }
 
